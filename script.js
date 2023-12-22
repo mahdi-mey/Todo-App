a52278168f24e6a5006b583bf49791338b800737
Mahdi <mahdimey17@gmail.com>
2023-12-22 18:17:57 +0330

    selected elements as variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const $ = document
+const inputElement  = $.querySelector('form-control')
+const formElement   = $.querySelector('.add')
+const parentElement = $.querySelector('.todo-container')
+const userInput     = $.querySelector('#user-input')
+const clearBtn      = $.querySelector('.userBtn')
