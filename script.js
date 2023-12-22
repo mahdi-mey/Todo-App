e80a965e9f88e181f0ebd12131a4e408ab70ce0d
Mahdi <mahdimey17@gmail.com>
2023-12-22 18:41:41 +0330

    addNewTodo function added

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const $ = document
-const inputElement  = $.querySelector('form-control')
+const inputElement  = $.querySelector('.form-control')
 const formElement   = $.querySelector('.add')
 const parentElement = $.querySelector('.todo-container')
 const userInput     = $.querySelector('#user-input')
 const clearBtn      = $.querySelector('.userBtn')
+
+function addNewTodo(title){
+    console.log(title);
+}
+
+inputElement.addEventListener('keydown', (event) => {
+    let newTodoValue = inputElement.value
+
+    if (event.keyCode === 13){
+        addNewTodo(newTodoValue)
+        inputElement.value = ''
+    }
+})
