9415e2d9eb786ba85e63a0f920c981c9c6fc4dbe
Mahdi <mahdimey17@gmail.com>
2023-12-26 21:15:45 +0330

    added todo add functionality

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 const $ = document
-const inputElement  = $.querySelector('.form-control')
-const formElement   = $.querySelector('.add')
+const inputElement = $.querySelector('.form-control')
+const formElement = $.querySelector('.add')
 const parentElement = $.querySelector('.todo-container')
-const userInput     = $.querySelector('#user-input')
-const clearBtn      = $.querySelector('.userBtn')
+const userInput = $.querySelector('#user-input')
+const clearBtn = $.querySelector('.userBtn')
 
-function addNewTodo(title){
-    console.log(title);
+let todosArray = []
+
+function addNewTodo(title) {
+    let newTodoObj = {
+        id: todosArray.length + 1,
+        title: title,
+        complete: false,
+    }
+    todosArray.push(newTodoObj)
+    generateTodo(newTodoObj)
+}
+
+function generateTodo(data) {
+    let newToDoLi = $.createElement('li');
+    newToDoLi.className = 'todo-item';
+
+    let newToDoSpan = $.createElement('span');
+    newToDoSpan.innerHTML = data.title;
+
+    let newToDoTrash = $.createElement('i');
+    newToDoTrash.className = 'fa fa-trash-o delete';
+    newToDoTrash.title = 'Delet item'
+
+    newToDoLi.append(newToDoSpan, newToDoTrash)
+
+    parentElement.append(newToDoLi)
+    newToDoTrash.setAttribute('onclick', `removeToDo(${data.id})`)
 }
 
 inputElement.addEventListener('keydown', (event) => {
     let newTodoValue = inputElement.value
 
-    if (event.keyCode === 13){
+    if (event.keyCode === 13) {
         addNewTodo(newTodoValue)
         inputElement.value = ''
     }
